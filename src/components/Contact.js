36ce550ccdde328600670defa0f0baa5264dff01
Mikiyas021 <122819257+Mikiyas021@users.noreply.github.com>
2024-01-25 00:28:51 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import contact from './images/contact.svg'
-import {faPhone} from '@fortawesome/free-solid-svg-icons';
-import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
 function Contact() {
   return (
     <div>
@@ -13,9 +11,9 @@ function Contact() {
         </div>
         <div className='contactinfo'>
             <div id='boxes'>
-                <div><p><FontAwesomeIcon icon={faPhone} /></p><p>0906449327</p></div>
-                <div><p><FontAwesomeIcon icon={faPhone} /></p><p>0984948339</p></div>
-                <div><p><FontAwesomeIcon icon={faEnvelope} /></p><p>mikiyas021@gmail.com</p></div>
+                <div><p><i class="fa-solid fa-phone"></i></p><p>0906449327</p></div>
+                <div><p><i class="fa-solid fa-phone"></i></p><p>0984948339</p></div>
+                <div><p><i class="fa-solid fa-envelope"></i></p><p>mikiyas021@gmail.com</p></div>
 
             </div>
         </div>
