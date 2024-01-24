3dd93e850dd8501eb1a2c2def054c4756680ce63
Mikiyas021 <122819257+Mikiyas021@users.noreply.github.com>
2024-01-25 00:34:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 import { createContext } from 'react';
 import All from './components/All.js';
 import { BrowserRouter } from 'react-router-dom';
-import About from './components/About.js';
-import contact from './components/Contact.js';
-import { Route,Routes } from 'react-router-dom';
 export const datacontext = createContext();
 function App() {
   return (
